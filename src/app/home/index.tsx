13bfd737e6aba178702aabd93f0ee10f2b791ef9
JuliaValadares <ju.valadares12@gmail.com>
2024-09-12 11:35:50 -0300

    seleção de contatos

--- src/app/home/index.tsx
+++ src/app/home/index.tsx
@@ -1,34 +1,51 @@
-import { View } from 'react-native'
+import { View , Alert } from 'react-native'
 import { Feather } from '@expo/vector-icons'
-import { theme } from "@/theme"
+import { theme } from "@/theme/index"
 import { styles } from './styles'
 import { Input } from '@/app/components/input'
+import { useState, useEffect } from 'react'
+import { Contact } from '../components/contact'
+import * as Contacts from 'expo-contacts'
 
-import{ useState } from 'react'
-import{ Contact} from '@/app/components/contact'
 
+async function fetchContacts() {
+    try{
+        const { status } = await Contacts.requestPermissionsAsync()
+        if (status === Contacts.PermissionStatus.GRANTED){
+            const { data } = await Contacts.getContactsAsync()
+            console.log(data)
+        }
+
+    } catch(error){
+        console.log(error)
+        Alert.alert("Contatos","Não foi possível carregar os contatos...")
+    }
+    
+
+}
 export function Home(){
     const [name, setName] = useState("")
-
-    return (
+    
+    useEffect(() =>{
+        fetchContacts()
+    },[])
+    
+    return ( 
         <View style={styles.container}>
             <View style={styles.header}>
                 <Input style={styles.input}>
-                <Feather name = "search" size={16}
-                color={theme.colors.gray_300}> 
-                </Feather>
-                <Input.Field
-                placeholder="Pesquisar pelo nome..."/>
-                <Feather name="x" size={16}
-                color={theme.colors.gray_300}></Feather>
+                    <Feather name="search" size={16}
+                    color={theme.colors.gray_300}></Feather>
+                    <Input.Field
+                    placeholder="Pesquisar pelo nome..." />
+                    <Feather name="x" size={16} 
+                    color={theme.colors.gray_300} onPress={() => setName("")}></Feather>
                 </Input>
             </View>
             <Contact contact={{
-            name: "Pedroso",
-            image: require("@/assets/avatar.jpeg")
-        }} />
+                name: "Julia",
+                image: require("@/assets/avatar.jpeg")
+            }} />
         </View>
-
-
     )
 }
--- src/app/home/styles.ts
+++ src/app/home/styles.ts
@@ -1,20 +1,19 @@
-import { StyleSheet } from "react-native";
+import {StyleSheet} from "react-native"
 import { theme } from "@/theme/index"
 
-
 export const styles = StyleSheet.create({
-    container:{
-        flex : 1,
+    container: {
+        flex: 1,
         backgroundColor: theme.colors.gray_200,
     },
     header: {
-        width:"100%",
+        width: "100%",
         height: 132,
         backgroundColor: theme.colors.blue,
         justifyContent: "flex-end",
         paddingHorizontal: 24,
     },
     input: {
-        marginBottom: -27,
+        marginBottom: 27,
     },
 })
